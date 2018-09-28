e984f94eba7a84860fbf62457b057d507aec3ff6
Magnus Holm <mail@magnusholm.com>
2018-09-28 17:15:31 +0100

    Add basic signup vc

---
+++ GlobeNotes/Views/Login/SignInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignInViewController.swift
+//  GlobeNotes
+//
+//  Created by magnus holm on 28/09/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import Foundation
---
+++ GlobeNotes/Views/Login/SignUpViewController.swift
@@ -0,0 +1,135 @@
+//
+//  SignUpViewController.swift
+//  GlobeNotes
+//
+//  Created by magnus holm on 28/09/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    fileprivate var addPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "addPhoto").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePhotoButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    fileprivate var emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        
+        return tf
+    }()
+    
+    fileprivate var usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "User name"
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        
+        return tf
+    }()
+    
+    fileprivate var passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isSecureTextEntry = true
+        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        
+        return tf
+    }()
+    
+    fileprivate var signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.lightGray
+        button.setTitle("Sign up", for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.isEnabled = false
+        
+        return button
+    }()
+    
+    // MARK: - ViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    // MARK: - Private methods
+    
+    fileprivate func setupUI() {
+        
+        view.backgroundColor = .white
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        view.addSubview(stackView)
+        
+        view.addSubview(addPhotoButton)
+        
+        setupConstraints()
+    }
+    
+    fileprivate func setupConstraints() {
+        addPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 60, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
+        addPhotoButton.anchor(centerX: view.centerXAnchor, centerY: nil)
+        
+        stackView.anchor(top: addPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+    }
+    
+    @objc fileprivate func handlePhotoButton() {
+        
+    }
+    
+    @objc fileprivate func handleTextInputChange() {
+        
+    }
+    
+    @objc fileprivate func handleSignUp() {
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
