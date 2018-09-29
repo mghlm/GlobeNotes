555e8cedcb537d94b28e5b37a49a446057ee323c
Magnus Holm <mail@magnusholm.com>
2018-09-29 16:36:17 +0100

    Setup fetching of city and country from coordinates in note tv cell

--- GlobeNotes/Views/HomeScreen/Views /NoteTableViewCell.swift
+++ GlobeNotes/Views/HomeScreen/Views /NoteTableViewCell.swift
@@ -7,35 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NoteTableViewCell: UITableViewCell {
     
-    // MARK: - Private properties
+    // MARK: - Public properties
     
-    fileprivate var note: Note! 
+    var note: Note! {
+        didSet {
+            setupUI()
+        }
+    }
+    
+    // MARK: - Private properties
     
     fileprivate var noteTitleLabel: UILabel = {
         let lbl = UILabel()
-        
+        lbl.font = UIFont.boldSystemFont(ofSize: 14)
         return lbl
     }()
     
     fileprivate var noteTextLabel: UILabel = {
         let lbl = UILabel()
-        
         return lbl
     }()
     
     fileprivate var locationLabel: UILabel = {
         let lbl = UILabel()
-        
         return lbl
     }()
     
-    // MARK: - UITableViewCell
-    
-    
-    
     // MARK: - Private methods
+    
+    fileprivate func setupUI() {
+        noteTitleLabel.text = note.title
+        noteTextLabel.text = note.text
+        setupLocationLabel()
+        addSubview(noteTitleLabel)
+        addSubview(noteTextLabel)
+        addSubview(locationLabel)
+        
+        setupConstraints()
+    }
+    
+    fileprivate func setupConstraints() {
+        
+    }
+    
+    fileprivate func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country: String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            completion(placemarks?.first?.locality, placemarks?.first?.country, error)
+        }
+    }
+    
+    fileprivate func setupLocationLabel() {
+        let location = CLLocation(latitude: note.latitude, longitude: note.longitude)
+        fetchCityAndCountry(from: location) { (city, country, error) in
+            guard let city = city else { return }
+            guard let country = country else { return }
+            
+            self.locationLabel.text = "\(city), \(country)"
+        }
+    }
 
 }
