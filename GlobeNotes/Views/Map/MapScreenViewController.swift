506307fcac50a928695807e2104987427f1bffff
Magnus Holm <mail@magnusholm.com>
2018-10-01 15:02:13 +0100

    Setup basic ui

---
+++ GlobeNotes/Views/Map/MapScreenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapScreenViewController.swift
+//  GlobeNotes
+//
+//  Created by magnus holm on 01/10/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+final class MapScreenViewController: UIViewController {
+    
+    // MARK: - Private properties
+    
+    fileprivate var mapView: MKMapView!
+    
+    // MARK: - ViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView = MKMapView()
+        view.addSubview(mapView)
+        
+        setupUI()
+    }
+    
+    // MARK: - Private methods
+    
+    fileprivate func setupUI() {
+        
+        setupConstraints()
+    }
+    
+    fileprivate func setupConstraints() {
+        mapView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+}
