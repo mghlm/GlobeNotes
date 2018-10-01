a0c8f988ae4104cfc474f6b80e245a1755926f2b
Magnus Holm <mail@magnusholm.com>
2018-10-01 17:33:50 +0100

    WIP Setup location manager w. relevant methods

--- GlobeNotes/Services/LocationManager.swift
+++ GlobeNotes/Services/LocationManager.swift
@@ -8,9 +8,17 @@
 
 import Foundation
 import CoreLocation
+import UIKit
 
 protocol LocationManagerType {
     
+    var usersCurrentLocation: CLLocation! { get }
+    
+    func startUpdatingLocation()
+    
+    func stopUpdatingLocation()
+    
+    func requestWhenInUseAuthorization()
 }
 
 class LocationManager: NSObject, LocationManagerType {
@@ -23,6 +31,8 @@ class LocationManager: NSObject, LocationManagerType {
         return lm
     }()
     
+    var usersCurrentLocation: CLLocation!
+    
     // MARK: - Initialization
     
     override init() {
@@ -30,8 +40,29 @@ class LocationManager: NSObject, LocationManagerType {
         
         locationManager.delegate = self
     }
+    
+    // MARK: - Public methods
+    
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func requestWhenInUseAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        usersCurrentLocation = locations.first
+    }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to fetch location:", error)
+    }
 }
