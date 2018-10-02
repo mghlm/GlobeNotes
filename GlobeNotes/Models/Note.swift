a953fa8277073ae1288882ffb23df93ced64fc54
Magnus Holm <mail@magnusholm.com>
2018-10-02 18:13:43 +0100

    Setup basic UI

--- GlobeNotes/Models/Note.swift
+++ GlobeNotes/Models/Note.swift
@@ -15,6 +15,7 @@ struct Note {
     let user: User?
     let title: String
     let text: String
+    let imageUrl: String?
     let latitude: Double?
     let longitude: Double?
     let creationDate: Date
@@ -23,6 +24,7 @@ struct Note {
         self.user = user
         self.title = dictionary["title"] as? String ?? ""
         self.text = dictionary["text"] as? String ?? ""
+        self.imageUrl = dictionary["imageUrl"] as? String 
         self.latitude = dictionary["latitude"] as? Double ?? 0
         self.longitude = dictionary["longitude"] as? Double ?? 0
         self.creationDate = Date()
