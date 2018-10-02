c8925e28f6485b20cae8a74aaa87061185505f53
Magnus Holm <mail@magnusholm.com>
2018-10-02 10:22:58 +0100

    Add NoteAnnotation

---
+++ GlobeNotes/Views/Map/NoteAnnotation.swift
@@ -0,0 +1,21 @@
+//
+//  NoteAnnotation.swift
+//  GlobeNotes
+//
+//  Created by magnus holm on 02/10/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import MapKit
+
+class NoteAnnotation: NSObject, MKAnnotation {
+    let title: String?
+    let subtitle: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+}
