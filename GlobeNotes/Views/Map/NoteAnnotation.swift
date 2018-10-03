ff15432fa9a713efa2e94afbbefe7e533f1edb19
Magnus Holm <mail@magnusholm.com>
2018-10-03 14:45:00 +0100

    Refactor note annotation subtitle

--- GlobeNotes/Views/Map/NoteAnnotation.swift
+++ GlobeNotes/Views/Map/NoteAnnotation.swift
@@ -15,7 +15,7 @@ class NoteAnnotation: NSObject, MKAnnotation {
     
     init(note: Note) {
         self.title = note.title
-        self.subtitle = "Created \(note.creationDate.timeIntervalSince1970) by \(note.userName ?? "unkown user")"
+        self.subtitle = note.userName != nil ? "Note created by \(note.userName ?? "")" : ""
         self.coordinate = CLLocationCoordinate2D(latitude: note.latitude ?? 0, longitude: note.longitude ?? 0)
     }
 }
